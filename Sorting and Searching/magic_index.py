321b68ca40b0889c422e99702b13f957257c0283
Rohit <rohitmis123@gmail.com>
2019-05-05 19:19:53 -0500

    Update magic_index.py

--- Sorting and Searching/magic_index.py
+++ Sorting and Searching/magic_index.py
@@ -12,6 +12,23 @@ def magic_index(arr):
   
   return _magic_index(0, len(arr) - 1)
 
+def magic_index_repeats(arr):
+  def _magic_index(l, r):
+    mid = (l + r) // 2
+    if mid == arr[mid]:
+      return mid
+    elif l > r:
+      return -1
+    right = _magic_index(mid + 1, r)
+    if right == -1:
+      return _magic_index(l, mid - 1)
+    else:
+      return right
+  
+  return _magic_index(0, len(arr) - 1)
 
 arr = [-1, 0, 1, 2, 4, 6, 8, 10]
 print(magic_index(arr))
+
+arr = [-1, 0, 1, 4, 4, 4, 8, 10]
+print(magic_index_repeats(arr))
